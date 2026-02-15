2be14dc3129353cd29cd819191206fad929ef969
LahaLuhem <ahalmehul+github@gmail.com>
2026-02-15 10:09:09 +0100

    more performant listenable builder for frequent rebuilds

--- lib/src/multi_value_listenable_builder.dart
+++ lib/src/multi_value_listenable_builder.dart
@@ -1,39 +1,57 @@
-import 'package:flutter/foundation.dart' show Key, Listenable, ValueListenable;
-import 'package:flutter/material.dart'
-    show AnimatedBuilder, BuildContext, StatelessWidget, Widget;
+import 'package:flutter/foundation.dart' show Listenable, ValueListenable, listEquals;
+import 'package:flutter/material.dart';
 
 /// This widget listens to multiple [ValueListenable]s and
 /// calls given builder function if any one of them changes.
-class MultiValueListenableBuilder extends StatelessWidget {
+class MultiValueListenableBuilder extends StatefulWidget {
   /// List of [ValueListenable]s to listen to.
   final List<ValueListenable> valueListenables;
 
   /// The builder function to be called when value of any of the [ValueListenable] changes.
   /// The order of values list will be same as [valueListenables] list.
-  final Widget Function(
-      BuildContext context, List<dynamic> values, Widget? child) builder;
+  final Widget Function(BuildContext, List<dynamic>, Widget?) builder;
 
-  /// An optional child widget which will be avaliable as child parameter in [builder].
+  /// An optional child widget which will be available as child parameter in [builder].
   final Widget? child;
 
-  // The const constructor.
+  /// Creates a [MultiValueListenableBuilder] widget.
   const MultiValueListenableBuilder({
     Key? key,
     required this.valueListenables,
     required this.builder,
     this.child,
-  })  : assert(valueListenables.length != 0),
-        super(key: key);
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: Listenable.merge(valueListenables),
-      builder: (context, child) {
-        final list = valueListenables.map((listenable) => listenable.value);
-        return builder(context, List<dynamic>.unmodifiable(list), child);
-      },
-      child: child,
-    );
+  State<MultiValueListenableBuilder> createState() => _MultiValueListenableBuilderState();
+}
+
+class _MultiValueListenableBuilderState extends State<MultiValueListenableBuilder> {
+  late Listenable _mergedListenable;
+
+  @override
+  void initState() {
+    super.initState();
+
+    assert(widget.valueListenables.isNotEmpty, 'valueListenables must not be empty');
+    _mergedListenable = Listenable.merge(widget.valueListenables);
   }
+
+  @override
+  void didUpdateWidget(covariant MultiValueListenableBuilder oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (listEquals(oldWidget.valueListenables, widget.valueListenables)) return;
+
+    _mergedListenable = Listenable.merge(widget.valueListenables);
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedBuilder(
+        animation: _mergedListenable,
+        builder: (context, child) {
+          final values = widget.valueListenables.map((v) => v.value).toList();
+          return widget.builder(context, values, child);
+        },
+        child: widget.child,
+      );
 }
